274f4ae5bee3b0d10bd79d2e8ac91df0b15868ac
Charles Gordinier III <cwgiii@udel.edu>
2024-02-25 16:36:00 -0500

    Complete with optional chaining syntax errors

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -1,10 +1,6 @@
 import Phaser from "phaser";
 
 export default class PreloadScene extends Phaser.Scene {
-    private platforms?: Phaser.Physics.Arcade.StaticGroup;
-    private player?: Phaser.Physics.Arcade.Sprite;
-    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
-
     constructor() {
         super({ key: "PreloadScene" });
     }
@@ -21,71 +17,6 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(400, 300, "sky");
-        this.platforms = this.physics.add.staticGroup();
-        const ground = this.platforms.create(
-            400,
-            560,
-            "ground"
-        ) as Phaser.Physics.Arcade.Sprite;
-        ground.setScale(2).refreshBody();
-        this.platforms.create(600, 400, "ground");
-        this.platforms.create(50, 250, "ground");
-        this.platforms.create(750, 220, "ground");
-
-        this.player = this.physics.add.sprite(100, 450, "dude");
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-
-        this.anims.create({
-            key: "left",
-            frames: this.anims.generateFrameNumbers("dude", {
-                start: 0,
-                end: 3,
-            }),
-            frameRate: 10,
-            repeat: -1,
-        });
-
-        this.anims.create({
-            key: "turn",
-            frames: [{ key: "dude", frame: 4 }],
-            frameRate: 20,
-        });
-
-        this.anims.create({
-            key: "right",
-            frames: this.anims.generateFrameNumbers("dude", {
-                start: 5,
-                end: 8,
-            }),
-            frameRate: 10,
-            repeat: -1,
-        });
-
-        this.physics.add.collider(this.player, this.platforms);
-
-        this.cursors = this.input.keyboard?.createCursorKeys();
-
         this.scene.start("MainScene");
-    }
-    update() {
-        if (this.cursors) {
-            return;
-        }
-        if (this.cursors.left?.isDown) {
-            this.player?.setVelocityX(-160);
-            this.player?.anims.play("left", true);
-        } else if (this.cursors.right?.isDown) {
-            this.player?.setVelocityX(160);
-            this.player?.anims.play("right", true);
-        } else {
-            this.player?.setVelocity(0);
-            this.player?.anims.play("turn");
-        }
-
-        if (this.cursors.up?.isDown && this.player?.body?.touching.down) {
-            this.player.setVelocityY(-330);
-        }
     }
 }
